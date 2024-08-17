c637ff024ec285432a2df66bb6ba2f0e658805db
Rene Myrie <rl.myrie@gmail.com>
2024-08-17 13:27:02 -0400

    Added script to refresh google calendar access token

---
+++ backend/get-token.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  authorize(JSON.parse(content), getAccessToken);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then call the
+ * callback function to get a new access token.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile('token.json', (err, token) => {
+    if (err) return callback(oAuth2Client);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and
+ * then call the callback with the authorized OAuth2 client.
+ */
+function getAccessToken(oAuth2Client) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/calendar'],
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile('token.json', JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', 'token.json');
+      });
+    });
+  });
+}
